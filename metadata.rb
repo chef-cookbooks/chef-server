bb876d9f63e6160571f7a35b0748dd888a4fef05
jtimberman <joshua@opscode.com>
2012-12-17 12:22:47 -0700

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures Chef Server"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.1.0"
+version           "1.1.1"
 recipe            "chef-server", "Compacts the Chef Server CouchDB."
 recipe            "chef-server::rubygems-install", "Set up rubygem installed chef server."
 recipe            "chef-server::apache-proxy", "Configures Apache2 proxy for API and WebUI"
