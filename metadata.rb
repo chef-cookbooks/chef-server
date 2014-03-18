86c0032ea1303c387ef9179b8d5fb1a4ffd47170
Sean OMeara <someara@opscode.com>
2014-03-18 19:01:15 -0400

    Version bump to 2.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs and configures Chef Server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.1.1'
+version           '2.1.2'
 
 supports 'centos'
 supports 'amazon'
