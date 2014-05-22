e4db7e6f6296aeba21da1211a56f72216079a7e5
Christopher Webber <cwebber@getchef.com>
2014-05-22 11:00:30 -0700

    Version bump to 2.1.7 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs and configures Chef Server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.1.6'
+version           '2.1.7'
 
 supports 'centos'
 supports 'amazon'
