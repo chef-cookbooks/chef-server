d0fdc978587314296781010bd0949b534bf1c053
Sean OMeara <sean@chef.io>
2015-04-09 22:30:49 -0400

    refactoring metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,13 @@
 name 'chef-server'
+version '3.1.1'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures Chef Server 12'
-version '3.1.1'
+source_url 'https://github.com/chef-cookbooks/chef-server'
+issues_url 'https://github.com/chef-cookbooks/chef-server/issues'
+
+depends 'chef-server-ingredient'
 
 supports 'centos'
 supports 'ubuntu'
-
-depends 'chef-server-ingredient'
