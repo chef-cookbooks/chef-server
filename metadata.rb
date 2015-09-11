0fc524259d524d68c86255a0d6e8965295b4c61e
Tim Smith <tsmith84@gmail.com>
2015-09-10 21:54:50 -0700

    Add long_description to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs and configures Chef Server 12'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/chef-cookbooks/chef-server' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/chef-server/issues' if respond_to?(:issues_url)
 
