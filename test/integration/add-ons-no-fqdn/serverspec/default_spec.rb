509be2b69f5502f620c53b14485703335f688195
Tim Smith <tsmith84@gmail.com>
2016-04-12 11:09:24 -0700

    Fix tests for add ons and add testing to travis

--- test/integration/add-ons-no-fqdn/serverspec/default_spec.rb
+++ test/integration/add-ons-no-fqdn/serverspec/default_spec.rb
@@ -1,11 +1,23 @@
 require_relative './spec_helper'
 
 describe 'chef-server' do
-  describe package('opscode-manage') do
+  describe package('chef-server-core') do
     it { should be_installed }
   end
 
-  describe command('opscode-manage-ctl test') do
+  describe package('chef-manage') do
+    it { should be_installed }
+  end
+
+  describe package('chef-sync') do
+    it { should be_installed }
+  end
+
+  describe command('chef-manage-ctl status') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe command('sudo chef-sync-ctl sync-status') do
     its(:exit_status) { should eq 0 }
   end
 end
