fe0942adf68e5caf9446d00439950495669c1c83
Tim Smith <tsmith@chef.io>
2016-08-31 16:21:32 -0700

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/addons_spec.rb
+++ spec/addons_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'chef-server::addons' do
   cached(:subject) do
     ChefSpec::SoloRunner.new do |node|
-      node.set['chef-server']['addons'] = %w(manage reporting)
+      node.normal['chef-server']['addons'] = %w(manage reporting)
     end.converge(described_recipe)
   end
 
