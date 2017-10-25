5939e8bc51b2a2d4a6f99cfdd544efce7a1b478f
Tim Smith <tsmith@chef.io>
2017-10-25 15:29:49 -0700

    Remove the test from the old sync install
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/add-ons-no-fqdn/default_spec.rb
+++ test/integration/add-ons-no-fqdn/default_spec.rb
@@ -10,8 +10,4 @@ control 'chef-server' do
   describe command('chef-manage-ctl status') do
     its(:exit_status) { should eq 0 }
   end
-
-  describe command('sudo chef-sync-ctl sync-status') do
-    its(:exit_status) { should eq 0 }
-  end
 end
