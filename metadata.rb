ffd890f47af5882a4d9f82458baa0ef1d0609add
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:01:59 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs and configures Chef Server 12'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 depends 'chef-ingredient', '>= 2.1.10'
 
 supports 'redhat'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook:: chef-server
 # Recipe:: default
 #
-# Copyright:: 2015-2016, Chef Software, Inc.
+# Copyright:: 2015-2019, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
